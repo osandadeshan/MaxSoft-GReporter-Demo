86d03bc69298684558c04ebbb53167ba132940d1
Osanda Deshan <osanda.deshan@gmail.com>
2018-06-24 21:43:36 +0530

    Bar chart and email styling changed using table views.

---
+++ src/test/java/com/maxsoft/emailclient/demo/EmailSender.java
@@ -0,0 +1,23 @@
+/**
+ * Project Name : MaxSoft Email Client For Gauge
+ * Developer    : Osanda Deshan
+ * Version      : 1.0.0
+ * Date         : 6/23/2018
+ * Time         : 2:56 PM
+ * Description  :
+ **/
+
+package com.maxsoft.emailclient.demo;
+
+import com.maxsoft.emailclient.Email;
+import com.maxsoft.emailclient.JsonReader;
+
+
+public class EmailSender {
+
+    public static void main(String[] args) {
+        Email.send(JsonReader.getExecutionResults());
+    }
+
+
+}
